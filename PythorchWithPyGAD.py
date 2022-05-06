d11498572cc685d7058f4d5d45f3f20d3e09e514
Roberto Dorata <roberto@dorata.eu>
2022-05-06 15:32:27 +0200

    testing sonarqube on github actions

--- PythorchWithPyGAD.py
+++ PythorchWithPyGAD.py
@@ -90,3 +90,6 @@ print("Predictions : \n", predictions.detach().numpy())
 
 abs_error = loss_function(predictions, data_outputs)
 print("Absolute Error : ", abs_error.detach().numpy())
+
+#test github actions sonarqube
+a = "test sonarqube"
